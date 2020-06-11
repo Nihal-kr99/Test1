8e9efebec0fc7d0b718e8a8715248782bda00317
Nihal-kr99 <nihalkumar.kr@gmail.com>
2020-06-11 17:29:57 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Array1
+{
+	public static void main(String[] args)
+	{
+		int n;
+		int avg=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of students");
+		n=sc.nextInt();
+		int marks[]=new int[n];
+		
+		
+		
+		System.out.println("Enter students marks");
+		for(int i=0;i<n;i++)
+		{
+			marks[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			avg=avg+marks[i];
+			
+		}
+		avg=avg/n;
+		System.out.println("Average marks are=" +avg);
+		
+		
+	}
+}
